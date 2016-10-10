57d35fea51d8ef1c9533f25f96b67fe4b9255659
503945930 <503945930@qq.com>
2016-10-10 22:20:23 +0800

    ejx

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -38,7 +38,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-  log4js.error("test");
+// configuration in all env
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+app.engine('html', require('ejs-mate'));
+app.locals._layoutFile = 'layout.html';
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
